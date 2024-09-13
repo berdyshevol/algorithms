a60ace3cde023e00d33965ce86cd77a3e7e9d1bf
berdyshevol <berdyshevo@gmail.com>
2024-09-13 11:38:26 -0500

    added tests

--- Binary Search/tests/1.test.js
+++ Binary Search/tests/1.test.js
@@ -1,12 +1,28 @@
-const { describe, test, expect } = require('@jest/globals')
-const binarySearch = require('../1 Binary Search')
+const { describe, test, expect } = require('@jest/globals');
+const binarySearch = require('../1 Binary Search');
 
 describe('binarySearch', () => {
   test('should return the index of the target if it exists in the array', () => {
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    const target = 3
-    const result = binarySearch(arr, target)
-    const expected = 2
-    expect(result).toBe(expected)
-  })
-})
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const target = 3;
+    const result = binarySearch(arr, target);
+    const expected = 2;
+    expect(result).toBe(expected);
+  });
+
+  test('should return -1 if the target does not exist in the array', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const target = 11;
+    const result = binarySearch(arr, target);
+    const expected = -1;
+    expect(result).toBe(expected);
+  });
+
+  test('should return -1 if the array is empty', () => {
+    const arr = [];
+    const target = 1;
+    const result = binarySearch(arr, target);
+    const expected = -1;
+    expect(result).toBe(expected);
+  });
+});
